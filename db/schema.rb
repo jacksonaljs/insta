ea010eedf50183cda34a876b0c2386045fdd435a
Jackson <jacksonaljs@gmail.com>
2018-12-10 18:21:24 +0530

    Changed DB from sql to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_08_07_090056) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
